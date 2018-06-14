99b500f13b2dd8e53a5166fb1561f28400e75f16
Alexander Courtis <acourtis@atlassian.com>
2018-06-14 17:27:19 +1000

    docker sickrage,sabnzbd is cronable, only restarts if there is an update and cleans up

---
+++ usr/local/bin/docker-sabnzbd.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# check for update
+docker pull linuxserver/sabnzbd | grep "Downloaded" >/dev/null
+if [ $? -eq 0 ]; then
+
+    # stop existing
+    docker stop sabnzbd
+
+    # delete existing
+    docker rm sabnzbd
+
+    # create new
+    docker create \
+        --name=sabnzbd \
+        --net=host \
+        -v /opt/sabnzbd/config:/config \
+        -v /opt/sabnzbd/incomplete-downloads:/incomplete-downloads \
+        -v /data/download:/download \
+        -e PGID=$(id -u sabnzbd) -e PUID=$(id -g sabnzbd) \
+        -e TZ=Australia/Sydney \
+        --restart unless-stopped \
+        linuxserver/sabnzbd
+
+    # add the sabnzbd process owner "abc" to the host's group "download"
+    cat << EOF > /tmp/15-addgroups
+#!/usr/bin/with-contenv bash
+groupadd -g $(id -g download) download
+gpasswd -a abc download
+EOF
+docker cp /tmp/15-addgroups sabnzbd:/etc/cont-init.d
+
+    # start new
+    docker start sabnzbd
+
+    # clean up
+    docker system prune -f
+else
+
+    # nothingtodohere
+    echo "no update available"
+fi
---
+++ usr/local/bin/docker-sickrage.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# check for update
+docker pull linuxserver/sickrage | grep "Downloaded" >/dev/null
+if [ $? -eq 0 ]; then
+
+    # stop existing
+    docker stop sickrage
+
+    # delete existing
+    docker rm sickrage
+
+    # create new
+    docker create \
+        --name=sickrage \
+        --net=host \
+        -v /opt/sickrage/config:/config \
+        -v /data/download:/downloads \
+        -v /data/tv:/tv \
+        -e PGID=$(id -u sickrage) -e PUID=$(id -g sickrage) \
+        -e TZ=Australia/Sydney \
+        --restart unless-stopped \
+        linuxserver/sickrage
+
+    # add the sickrage process owner "abc" to the host's groups "download" and "tv"
+    cat << EOF > /tmp/15-addgroups
+#!/usr/bin/with-contenv bash
+groupadd -g $(id -g download) download
+groupadd -g $(id -g tv) tv
+gpasswd -a abc download
+gpasswd -a abc tv
+EOF
+    docker cp /tmp/15-addgroups sickrage:/etc/cont-init.d
+    
+    # start new
+    docker start sickrage
+
+    # clean up
+    docker system prune -f
+else
+
+    # nothingtodohere
+    echo "no update available"
+fi
