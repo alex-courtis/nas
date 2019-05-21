385d2c38ccf58e71de3e6d9921979cd6fea10eef
Alexander Courtis <alex@courtis.org>
2019-05-21 17:56:44 +1000

    migrate from sickrage docker image to sickchill

--- usr/local/bin/docker-sickchill.sh
+++ usr/local/bin/docker-sickchill.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 # check for update
-docker pull linuxserver/sickrage | grep "Downloaded" >/dev/null
+docker pull linuxserver/sickchill | grep "Downloaded" >/dev/null
 if [ $? -eq 0 ]; then
 
     # stop existing
-    docker stop sickrage
+    docker stop sickchill
 
     # delete existing
-    docker rm sickrage
+    docker rm sickchill
 
     # create new
     docker create \
-        --name=sickrage \
+        --name=sickchill \
         --net=host \
-        -v /opt/sickrage/config:/config \
+        -v /opt/sickchill/config:/config \
         -v /download:/downloads \
         -v /tv:/tv \
-        -e PGID=$(id -u sickrage) -e PUID=$(id -g sickrage) \
+        -e PGID=$(id -u sickchill) -e PUID=$(id -g sickchill) \
         -e TZ=Australia/Sydney \
         --restart unless-stopped \
-        linuxserver/sickrage
+        linuxserver/sickchill
 
-    # add the sickrage process owner "abc" to the host's groups "download" and "tv"
+    # add the sickchill process owner "abc" to the host's groups "download" and "tv"
     cat << EOF > /tmp/15-addgroups
 #!/usr/bin/with-contenv bash
 groupadd -g $(id -g download) download
@@ -30,10 +30,10 @@ groupadd -g $(id -g tv) tv
 gpasswd -a abc download
 gpasswd -a abc tv
 EOF
-    docker cp /tmp/15-addgroups sickrage:/etc/cont-init.d
+    docker cp /tmp/15-addgroups sickchill:/etc/cont-init.d
     
     # start new
-    docker start sickrage
+    docker start sickchill
 
     # clean up
     docker system prune -f
