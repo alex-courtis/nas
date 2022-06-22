53432d02aedd9d67b374c5847fadb2ccfc1de7ba
Alexander Courtis <alex@courtis.org>
2022-06-22 17:55:46 +1000

    accept that plex docker container needs host networking

--- usr/local/bin/docker-plex.sh
+++ usr/local/bin/docker-plex.sh
@@ -15,8 +15,8 @@ if [ $? -eq 0 ]; then
     # create new
     docker create \
         --name=plex \
-        -p 32400:32400 \
-        -e VERSION=public \
+		--net=host \
+        -e VERSION=docker \
         -e PUID=$(id -u plex) -e PGID=$(id -g plex) \
         -e TZ=Australia/Sydney \
         -v /opt/plex/config:/config \
