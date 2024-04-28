ac009f910eba2b2e0b7f89594268d15b7167a6a2
Alexander Courtis <alex@courtis.org>
2024-04-28 16:27:18 +1000

    ext4 raid, msmtp->dma

---
+++ raid.sh
@@ -0,0 +1,81 @@
+pacman -S mdadm arch-install-scripts
+
+
+#
+# remove
+#
+
+umount /dev/md0
+mdadm -S /dev/md0
+for disk in a b c d; do
+	mdadm --misc --zero-superblock "/dev/sd${disk}1"
+done
+
+
+#
+# partitions
+#
+for disk in a b c d; do
+	wipefs --all "/dev/sd${disk}"
+	parted --script "/dev/sd${disk}" \
+		mktable GPT \
+		mkpart raid 1MiB 100% \
+		type 1 A19D880F-05FC-4D3B-A006-743F0F84911E
+done
+
+
+#
+# raid device
+#
+
+# https://www.zdnet.com/article/chunks-the-hidden-key-to-raid-performance/
+mdadm --create --verbose \
+	--level=5 \
+	--metadata=1.2 \
+	--chunk=16 \
+	--raid-devices=4 /dev/md0 \
+	/dev/sda1 /dev/sdb1 /dev/sdc1 /dev/sdd1
+
+mdadm --detail /dev/md0
+
+cat /proc/mdstat # no need to wait for spare
+
+sysctl -w dev.raid.speed_limit_min=600000
+sysctl -w dev.raid.speed_limit_max=600000
+
+echo "dev.raid.speed_limit_min = 600000" > /etc/sysctl.d/raid.conf
+echo "dev.raid.speed_limit_max = 600000" >> /etc/sysctl.d/raid.conf
+
+# wait for build
+
+echo "MAILADDR none@example.com" >> /etc/mdadm.conf
+mdadm --detail --scan >> /etc/mdadm.conf
+
+mdadm --assemble --scan
+
+
+#
+# filesystem
+#
+
+# hardware block size, 4K
+blockdev --getbsz /dev/sda
+
+# stride = chunk size / block size = 16K / 4K = 4
+# stripe width = number of data disks * stride = 3 * 4 = 12
+
+mkfs.ext4 -v -L nfs -b 4096 -E stride=4,stripe-width=12 /dev/md0
+
+
+#
+# mount
+#
+
+mkdir /srv/nfs
+mount /dev/md0 /srv/nfs
+
+genfstab -U /srv/nfs >> /etc/fstab
+
+# remove swap, set last fsck to 2, change point to /srv/nfs
+vi /etc/fstab
+
