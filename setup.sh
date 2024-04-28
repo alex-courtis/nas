56a45daa5eb23367f38c977b7c7bfb7671d43dbe
Alexander Courtis <alex@courtis.org>
2024-04-28 18:52:37 +1000

    monit using gmail

--- setup.sh
+++ setup.sh
@@ -5,3 +5,16 @@ echo drivetemp > /etc/modules-load.d/drivetemp.conf
 modprobe drivetemp
 sensors
 
+#
+# monit
+#
+cp /etc/nas/etc/monitrc /etc
+systemctl enable monit
+systemctl start monit
+
+
+# TODO
+# raid maintenance
+# disk maintenance
+# 7 day systemd heartbeat timer
+printf "from: lord <alex@courtis.org>\nsubject: %s\n" "$(uptime -p)" | sendmail alex@courtis.org
