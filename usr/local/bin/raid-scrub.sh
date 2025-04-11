c4d396599cb03cfb2ac3806f3116374a67eab0da
Alexander Courtis <alex@courtis.org>
2025-04-11 16:13:16 +1000

    raid: reload sysctl params before scrub

--- usr/local/bin/raid-scrub.sh
+++ usr/local/bin/raid-scrub.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# reload, as sysctl starts before raid, see
+# lord systemd-sysctl[495]: Couldn't write '5000000' to 'dev/raid/speed_limit_min', ignoring: No such file or directory
+sysctl --system
+
 # mdcheck systemd service exists however this is enough
 
 echo check > /sys/block/md0/md/sync_action
