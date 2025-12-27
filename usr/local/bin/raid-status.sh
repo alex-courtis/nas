2a616d871d9753462afe5c664d2dff48762287a8
Alexander Courtis <alex@courtis.org>
2025-12-27 16:08:40 +1100

    add raid speed limits to reports

--- usr/local/bin/raid-status.sh
+++ usr/local/bin/raid-status.sh
@@ -3,4 +3,7 @@ cat << EOM
 /sys/block/md0/md/last_sync_action $(cat /sys/block/md0/md/last_sync_action)
 /sys/block/md0/md/mismatch_cnt     $(cat /sys/block/md0/md/mismatch_cnt)
 $(cat /proc/mdstat | sed -E 's/[<>]//g')
+$(sysctl dev.raid.speed_limit_min)
+$(sysctl dev.raid.speed_limit_max)
+group_thread_cnt=$(cat /sys/block/md0/md/group_thread_cnt)
 EOM
